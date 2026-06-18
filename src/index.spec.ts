c225a87f92651613e501f16a6dbcaa93882927bd
LloydLim1 <111641482+LloydLim1@users.noreply.github.com>
2026-06-19 01:50:18 +0800

    chore: initialize project scaffold

---
+++ src/index.spec.ts
@@ -0,0 +1,7 @@
+import { SERVICE_NAME } from './index';
+
+describe('testmultiv3-frontend', () => {
+  it('should export SERVICE_NAME', () => {
+    expect(SERVICE_NAME).toBe('testmultiv3-frontend');
+  });
+});
